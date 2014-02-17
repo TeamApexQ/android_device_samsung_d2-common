87a9cc45e11288ae32f2cb44af4370ec21bddc5b
lbdroid <lbdroid@users.noreply.github.com>
2014-02-17 15:36:22 -0500

    Torch

--- d2-common.mk
+++ d2-common.mk
@@ -73,7 +73,7 @@ PRODUCT_COPY_FILES += \
     device/samsung/d2-common/gps/gps.conf:system/etc/gps.conf
 
 # Torch
-PRODUCT_PACKAGES += Torch
+PRODUCT_PACKAGES += OmniTorch
 
 # Wifi
 PRODUCT_PACKAGES += \
