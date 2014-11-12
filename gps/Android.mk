a5ae1b6a7eb3dfc64e38189dec282149c38d13e8
lbdroid <lbdroid@users.noreply.github.com>
2014-11-12 16:07:19 -0500

    Away GPS

--- gps/Android.mk
+++ gps/Android.mk
@@ -24,6 +24,6 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(BOARD_VENDOR),samsung)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+#include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
 endif
